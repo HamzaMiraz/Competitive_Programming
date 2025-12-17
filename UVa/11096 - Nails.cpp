2f35dea1bb663f6dfc3a6922083fdba99f44a67f
HamzaMiraz <98533446+HamzaMiraz@users.noreply.github.com>
2025-12-17 16:45:37 +0600

    Add files via upload

---
+++ UVa/11096 - Nails.cpp
@@ -0,0 +1,186 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//For Debugging
+#define debug(a...)          {cout<<__LINE__<<" #--> ";dbg,a; cout<<endl;}
+struct debugger
+{
+    template<typename T> debugger& operator , (const T v)
+    {
+        cout<<v<<" ";
+        return *this;
+    }
+} dbg;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T>using iset=tree<T,null_type,less<T>,///descending_order=greater<T>,multi_iset=less_equal<T>
+rb_tree_tag,tree_order_statistics_node_update> ;///exmp= iset<double>st
+///who is k'th position= *(set.find_by_order(k)),index of v = set.order_of_key(v);
+
+typedef long long ll;/// 1e18;
+//typedef __int128_t LL;/// 1e32;
+template <typename T>
+T BIGMOD(T base,T power,T Mod=1e9+7){T ans = 1;
+while(power){if(power & 1)ans = (ans * base) % Mod;
+base = (base * base) % Mod;power = power >> 1;}return ans;}
+
+template <typename T>
+T INV(T base,T mod=1e9+7){//defult mod=1e9+7
+    return BIGMOD(base%mod,mod-2,mod)%mod;///base^-1
+}
+
+template <typename T>
+T nCr(T n, T r){T res=1;for (T i=0;i<r;i++){
+res *= (n-i);res /= (i+1);}return res;}/// NCR
+
+template <typename T>
+T fact(T n){if(n<=1)return 1;return n*fact(n-1);}
+
+template <typename T>
+T nPr(T n, T r){return fact(n)/fact(n-r);}///NPR
+
+
+#define deb(a)     cout<<__LINE__<<"# "<<#a<<" -> "<<a<<endl;
+#define all(x) x.begin(), x.end()
+#define loop(IV,INIT,NV) for(int IV=INIT;IV<=NV;IV++)
+#define lbv(vec,x) lower_bound(all(vec),x)-vec.begin()//retrun index
+#define ubv(vec,x) upper_bound(all(vec),x)-vec.begin()
+#define lba(ar,n,x) lower_bound(ar,ar+n,x)-&ar[0];//return index
+#define uba(ar,n,x) upper_bound(ar,ar+n,x)-&ar[0];
+#define lb(mp,x) mp.lower_bound(x);///for set,map
+#define ub(st,x) st.upper_bound(x);///return pointer address
+#define GCD(a,b)  __gcd(a,b)
+#define LCM(a,b)  (a*b)/__gcd(a,b)
+#define sqr(a)    ((a)*(a))
+#define deg(n)  n*PI/180 /// redian to degree
+#define sp(n,d)  fixed << setprecision(d) <<n
+#define pb emplace_back
+#define F  first
+#define S  second
+#define ss " "
+#define nn printf("\n");
+#define el cout<<"\n";
+#define yes cout<<"Yes\n";
+#define no cout<<"No\n";
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+
+template <typename T>
+using minHeap = priority_queue<T, vector<T>, greater<T>>;
+
+const double PI = acos(-1);
+const double EPS = 1e-7; ///1*10^-7
+const int oo = 1e9+10;
+const ll MOD = 1e9 +7;// Prime
+
+
+struct P
+{
+    ll x,y;
+    P(){
+        x=y=0;
+    }
+
+    P(ll x,ll y){
+        this->x=x;
+        this->y=y;
+    }
+
+    bool operator<(const P &b)const{
+        if(x!=b.x)  return x<b.x;
+        return y<b.y;
+    }
+
+    void scan(){cin>>x>>y;}
+    void print(){cout<<x<<"  "<<y<<endl;}
+};
+
+P pvt;
+
+P mv(P a,P b){return P(b.x-a.x,b.y-a.y);}
+ll cp(P a,P b){return a.x*b.y-a.y*b.x;}
+double sdis(P a,P b){return sqrt(sqr(a.x-b.x)+sqr(a.y-b.y));}
+
+bool comp(P a,P b)
+{
+    ll c = cp(mv(pvt,a),mv(pvt,b));
+
+    if(c)   return c>0;
+    return sdis(pvt,a)<sdis(pvt,b);
+
+}
+
+vector<P> makeConvexHull(vector<P> p)
+{
+    int nConvex=p.size(),i,j;
+
+    if(nConvex<4) return p;
+
+/**     IF ALL POINTS ARE CO-LINER   */
+
+//    j=0;
+//    for(i=2;i<nConvex;i++)
+//        if(cp(mv(p[0],p[1]),mv(p[0],p[i]))==0)
+//            j++;
+//    if(j+2==nConvex)  return P
+
+    pvt=p[0];
+    for(int i=1;i<nConvex;i++)
+        if(pvt.y>p[i].y)    pvt=p[i];
+        else if(pvt.y==p[i].y && pvt.x>p[i].x)    pvt=p[i];
+
+    sort(all(p),comp);
+
+    j=2;
+    for(int i=2;i<nConvex;i++)
+    {
+        while(j>1 && cp(mv(p[j-2],p[j-1]),mv(p[j-2],p[i]))<0)    j--;
+        p[j++]=p[i];
+    }
+    p.resize(j);
+    return p;
+}
+
+void solve() {
+    double L;
+    int n;
+    cin>>L>>n;
+
+    vector<P>v;
+    for (int i = 0; i < n; i++){
+        ll x,y;
+        cin>>x>>y;
+        v.push_back({x,y});
+    }
+    if(n==1){
+        cout<<sp(L,5)<<"\n";
+        return;
+    }
+    v=makeConvexHull(v);
+
+    double per =0;
+    for (int i =0;i<v.size();i++)
+        per+=sdis(v[i],v[(i+1)%v.size()]);
+
+    cout<<sp(max(L,per),5)<<"\n";
+}
+
+signed main()
+{
+#ifndef ONLINE_JUDGE
+     freopen("inputf.in", "r", stdin);  ///To read from a file.
+//    freopen("outputf.in", "w", stdout);  ///To write  a file.
+#endif
+    cin.tie(nullptr)->sync_with_stdio(false);
+    ll tks=1;
+    cin>>tks;
+    for(ll ks=1;ks<=tks;ks++){
+        //cout<<"Case "<<ks<<": ";
+        solve();
+    }
+    //cerr<<"\nTime:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+    return 0;
+}
+///invisible =  
